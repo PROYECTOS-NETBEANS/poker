65bfc81f4eb7b327ec85ddc7316f3ba14d931e87
optima2016 <limbertx@hotmail.com>
2016-11-22 10:40:22 -0400

    Escuchador de los eventos que se generan cuando llega un paquete del servidor

---
+++ poker/src/poker/cliente/negocio/OnPackageListener.java
@@ -0,0 +1,14 @@
+package poker.cliente.negocio;
+
+import java.util.EventListener;
+
+/**
+ * Interface escuchador para los eventos
+ * cuando llegan los paquetes del servidor.
+ * @author Alex Limbert Yalusqui <limbertyalusqui@gmail.com>
+ */
+public interface OnPackageListener extends EventListener{
+    public void listaMesas();
+    public void nuevaMesa();
+    public void mesaLlena();
+}
