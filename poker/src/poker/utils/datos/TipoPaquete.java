7e6d9d2c6245544615518bdae72977f823973c6b
win7 <limbertx@hotmail.net>
2016-12-13 09:50:43 -0400

    adicion de nuevas constantes

--- poker/src/poker/utils/datos/TipoPaquete.java
+++ poker/src/poker/utils/datos/TipoPaquete.java
@@ -12,5 +12,6 @@ public enum TipoPaquete {
     PAQUETE_VACIO,
     // es un paquete con el id de la mesa a donde uno ingresa para enviar al cliente
     // Tambien se usa para esperar la respuesta del servidor , devuelve un jugador y mesa donde ingreso
-    INGRESAR_A_MESA
+    INGRESAR_A_MESA,
+    MESSAGE,
 }
