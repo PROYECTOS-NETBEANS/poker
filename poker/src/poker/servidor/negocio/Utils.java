34abae994e3194d1b4de1c6787603886543beac8
win7 <limbertx@hotmail.net>
2016-12-13 09:48:50 -0400

    adicion de nuevas constantes

--- poker/src/poker/servidor/negocio/Utils.java
+++ poker/src/poker/servidor/negocio/Utils.java
@@ -1,29 +1,17 @@
 package poker.servidor.negocio;
 
-import poker.utils.datos.PaquetePk;
-import poker.utils.datos.Parser;
-import poker.utils.datos.TipoPaquete;
-
 /**
  * 
  * @author Alex Limbert Yalusqui <limbertyalusqui@gmail.com>
  */
 public class Utils {
 
-    /**
-     *Numero maximo de jugadores
-     */
     public static final int nroMaximoJugadores = 4;
-    
-    public Utils(){
-        
-    }
+    public static final int ciegaAlta = 100;
+    public static final int ciegaPequeña = 50;
+    public static final int nroMinimoJugadores = 3;
+
     public static void main(String[] arg){
-        Mesa m = new Mesa(2, 2, 100);
-        String stringg = "Hola mundo como estas";
-        PaquetePk p = new PaquetePk(stringg, TipoPaquete.MESA);
         
-        String data = Parser.objectToString(p);
-        System.out.println("mesa : " + data);
     }
 }
