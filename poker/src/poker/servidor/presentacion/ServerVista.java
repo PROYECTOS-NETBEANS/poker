2c09eed04c470481b3f0876d89122cf9218dc91e
win7 <limbertx@hotmail.net>
2016-12-13 09:51:25 -0400

    cambio de la vista server

--- poker/src/poker/servidor/presentacion/ServerVista.java
+++ poker/src/poker/servidor/presentacion/ServerVista.java
@@ -44,6 +44,8 @@ public class ServerVista extends javax.swing.JFrame implements OnConnectedListen
         jLabel1 = new javax.swing.JLabel();
         lblEstado = new javax.swing.JLabel();
         addMesa = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -67,16 +69,31 @@ public class ServerVista extends javax.swing.JFrame implements OnConnectedListen
             }
         });
 
+        jButton1.setText("jButton1");
+
+        jButton2.setText("jButton2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnIniciar)
-                    .addComponent(addMesa))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnIniciar)
+                            .addComponent(addMesa)
+                            .addComponent(jButton1)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addComponent(jButton2)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -99,7 +116,11 @@ public class ServerVista extends javax.swing.JFrame implements OnConnectedListen
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(72, 72, 72)
-                        .addComponent(addMesa)))
+                        .addComponent(addMesa)
+                        .addGap(35, 35, 35)
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
                 .addComponent(lblEstado)
                 .addGap(25, 25, 25))
@@ -115,6 +136,10 @@ public class ServerVista extends javax.swing.JFrame implements OnConnectedListen
     private void addMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addMesaActionPerformed
         server.nuevaMesa();
     }//GEN-LAST:event_addMesaActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        jButton1.setEnabled(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -155,6 +180,8 @@ public class ServerVista extends javax.swing.JFrame implements OnConnectedListen
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addMesa;
     private javax.swing.JButton btnIniciar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblEstado;
